8c0e2eb7f8763ca22805559744e59de632e23595
Uladzislau Charniauski <vlad.cherniavsky@gmail.com>
2025-02-26 15:34:44 +0300

    Add shadow component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
 import Header from './components/Header'
 import ShoeFinder from './components/ShoeFinder'
-import './index.css';
+import root from 'react-shadow';
+import styles from './index.css?inline'
+
+console.log('styles', styles)
 
 function App({label, questions}) {
 
   return (
-    <div className="min-h-screen bg-white">
+    <root.div className="min-h-screen bg-white">
       <p>props label: {label}</p>
       <p>props questions: {questions}</p>
       <Header />
       <ShoeFinder />
-    </div>
+      <style type="text/css">{styles}</style>
+    </root.div>
   )
 }
 
